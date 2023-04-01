cf39240ba0d7c25b0ff7c0b06828dee2753ff023
Sandro Rybarik <sandro.rybarik@gmail.com>
2023-04-01 14:39:25 +0200

    Cleaning + new favicon
    + removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
-import './App.css'
 import Exchange from './Exchange';
 
-// Create a client
 const queryClient = new QueryClient()
 
 function App() {
   return (
-    // Provide the client to your App
     <QueryClientProvider client={queryClient}>
       <Exchange />
     </QueryClientProvider>
